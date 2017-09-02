2ffca96b1400311386ad9da7efd8dd219f06f847
Kevinying <silverstart1987913@yahoo.com>
2017-09-02 16:29:09 +0200

    Commit

--- goodreads/models/BookUrl.py
+++ goodreads/models/BookUrl.py
@@ -10,11 +10,13 @@ class BookUrl(db.Model):
     book_title = db.Column(db.String(500))
     category_url = db.Column(db.String(500))
     book_id = db.Column(db.Integer(), primary_key=True)
+    page_no = db.Column(db.Integer())
     
-    def __init__(self, book_url, book_title, category_url):
+    def __init__(self, book_url, book_title, category_url, page_no):
         self.book_url = book_url
         self.category_url = category_url
         self.book_title =  book_title
+        self.page_no =  page_no
 
     # Return a nice JSON response
     def serialize(self):
@@ -23,4 +25,5 @@ class BookUrl(db.Model):
             'book_url': self.book_url,
             'book_title': self.book_title,
             'category_url': self.category_url,
+            'page_no': self.page_no,
         }
